fdcc481c0cd6ab6adf658eda49e13a61389b142c
Saifullah-Balghari <balgharisaifullah@gmail.com>
2024-02-17 23:20:44 +0500

    Initial Commit

--- 02.DataCleaning.py
+++ 02.DataCleaning.py
@@ -1,1 +1,12 @@
 import pandas as pd
+import os
+
+## Data Cleaning: Fixing bad data like: Empty cells, Data in wrong format, Wrong data and Duplicates
+
+path = os.path.join("Data", "UncleanedCSV.csv")
+df = pd.read_csv(path, encoding='latin1')
+print(df, "\n")
+
+# Removing rows that contains empty cells: dropna()
+print(df.dropna(), "\n")
+
