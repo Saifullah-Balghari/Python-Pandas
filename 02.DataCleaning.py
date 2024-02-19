90aa95460233e37a586c6919db52f71daf0b337a
Saifullah-Balghari <balgharisaifullah@gmail.com>
2024-02-19 12:51:21 +0500

    Initial Commit

--- 02.DataCleaning.py
+++ 02.DataCleaning.py
@@ -5,14 +5,19 @@ import os
 
 path = os.path.join("Data", "UncleanedCSV.csv")
 df = pd.read_csv(path, encoding='latin1')
-print(df, "\n")
 
 # Cleaning rows that contains empty cells
 df = df.dropna()
-print(df)
 
 # Cleaning wrong formats
+df['Date'] = df['Date'].str.replace("'", "")
+df['Date'] = pd.to_datetime(df['Date'], format='%Y/%m/%d')
 
-df = pd.read_csv(path)
-df['Date'] = pd.to_datetime(df['Date'], errors='coerce', format='%Y/%m/%d')
+# Replacing values
+df.loc[3, 'Duration'] = 45
+
+# Removing Duplicates
+df.drop_duplicates(inplace = True)
 print(df)
+df1 = df
+df1.to_csv("Data//CleanedCSV.csv", index=False)
